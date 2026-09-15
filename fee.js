294859fc14d25045e3312842d309657c4eefc19f
wesleycs626 <22174790@student.westernsydney.edu.au>
2026-09-15 16:29:48 +0700

    Wesley 22174790 fee.js update

--- fee.js
+++ fee.js
@@ -1,5 +1,18 @@
-function fee(d, r) {
-  let total = d * r;
-  console.log(total);
-  return total;
+
+
+// improved equipment loan fee calculation
+function calculateFee (borrowingDays,dailyRate) 
+{
+  //
+  if(!Number.isInteger(borrowingDays) || borrowingDays <= 0)
+  {
+      throw new Error ("Borrowing days only accepts positive int");
+  }
+  
+  if(!Number.isFinite(dailyRate) || dailyRate < 0)
+  {
+    throw new Error ("daily Rate only accepts finite and non negative);
+  }
+
+  return borrowingDays * dailyRate;
 }
